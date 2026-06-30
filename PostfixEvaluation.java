1823b4d46e6b9cd9ea9a5ee123ec3940e5e0cb66
dhachu136 <dhachu136@gmail.com>
2026-06-30 14:07:04 +0530

    Add files via upload

---
+++ PostfixEvaluation.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+public class PostfixEvaluation {
+
+    public static int evaluatePostfix(String exp) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (char ch : exp.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                stack.push(ch - '0');  
+            }
+            else {
+                int val2 = stack.pop();
+                int val1 = stack.pop();
+                switch (ch) {
+                    case '+':
+                        stack.push(val1 + val2);
+                        break;
+                    case '-':
+                        stack.push(val1 - val2);
+                        break;
+                    case '*':
+                        stack.push(val1 * val2);
+                        break;
+                    case '/':
+                        stack.push(val1 / val2);
+                        break;
+                }
+            }
+        }
+        return stack.pop();
+    }
+
+    public static void main(String[] args) {
+        String exp = "231*+9-"; 
+        System.out.println("Result: " + evaluatePostfix(exp));
+    }
+}
